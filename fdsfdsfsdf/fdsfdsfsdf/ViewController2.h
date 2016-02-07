ef40cf29714aa0fa4974804e83a8710dde919d81
Jose Pose Salgueiro <joseposesalgueiro@MacBook-Pro-de-Jose.local>
2016-02-07 12:55:38 +0100

    all

---
+++ fdsfdsfsdf/fdsfdsfsdf/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  fdsfdsfsdf
+//
+//  Created by Jose Pose Salgueiro on 7/2/16.
+//  Copyright © 2016 Jose Pose Salgueiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
---
+++ fdsfdsfsdf/fdsfdsfsdf/ViewController2.m
@@ -0,0 +1,37 @@
+//
+//  ViewController2.m
+//  fdsfdsfsdf
+//
+//  Created by Jose Pose Salgueiro on 7/2/16.
+//  Copyright © 2016 Jose Pose Salgueiro. All rights reserved.
+//
+
+#import "ViewController2.h"
+
+@interface ViewController2 ()
+
+@end
+
+@implementation ViewController2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
